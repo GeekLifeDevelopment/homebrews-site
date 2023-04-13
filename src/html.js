20e2fab6f746b8e6ba2de461745f2133a15e6ccf
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-04-12 23:19:14 -0500

    added font awesome script

--- src/html.js
+++ src/html.js
@@ -20,6 +20,14 @@ export default function HTML(props) {
           href='https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto&display=swap'
           rel='stylesheet'
         />
+
+        {/* Font Awesome */}
+        <link
+          rel='stylesheet'
+          href='https://use.fontawesome.com/releases/v5.5.0/css/all.css'
+          integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU'
+          crossOrigin='anonymous'
+        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
